e26784d3fb6f10c0bf73f6935e24c04224bb419b
Shkaff02 <78539412+Shkaff02@users.noreply.github.com>
2021-06-06 14:44:46 +0300

    Add form

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,7 +1,31 @@
 package main;
 
+
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import java.awt.*;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hi");
+        JFrame frame = new JFrame();
+        frame.setSize(600, 400);
+        frame.setTitle("Let's play!");
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(true);
+        frame.setLayout(new GridBagLayout());
+
+        JButton myButton = new JButton("Click");
+        JButton myButton2 = new JButton("Click Again");
+
+        myButton.setBackground(new Color(112, 95, 32));
+        myButton2.setForeground(Color.blue);
+        frame.add(myButton);
+        frame.add(myButton2);
+
     }
 }
+
